b718f2d7080e15ba6c0e9bf16f6950022dc4413d
reshmi4chat-ui <reshmi4chat@gmail.com>
2026-03-25 12:53:33 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("calc-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/app.js"
+      ]);
+    })
+  );
+});
